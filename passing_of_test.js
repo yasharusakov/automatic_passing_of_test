37605012d08795e43852fb65f9f8606fd12465aa
yasharusakov <rusakov.yasha@gmail.com>
2023-03-03 01:11:27 +0200

    it works but has some bugs

--- passing_of_test.js
+++ passing_of_test.js
@@ -11,59 +11,89 @@ async function passingOfTest() {
         await driver.findElement(By.id('joinform-name')).sendKeys(process.env.NAUROK_USERNAME, Key.ENTER)
         await driver.findElement(By.className('join-button-test')).click()
 
+        let currentQuestion = 1
+
+        await fn()
+
         // PASSING OF TEST
-        // GET QUESTION AND ANSWERS
-        const question = await driver.wait(until.elementLocated(By.css('.test-content-text-inner p'))).getText()
-        const elements = await driver.wait(until.elementsLocated(By.className('question-option-inner-content')))
-        const answers = []
+        async function fn() {
+            // GET QUESTION AND ANSWERS
+            const question = await driver.wait(until.elementLocated(By.css('.test-content-text-inner p'))).getText()
+            const elements = await driver.wait(until.elementsLocated(By.className('question-option-inner-content')))
+            const answers = []
 
-        await Promise.all(elements.map(async (element, i) => {
-            const paragraphs = await element.findElements(By.css('p'))
+            await Promise.all(elements.map(async (element, i) => {
+                const paragraphs = await element.findElements(By.css('p'))
 
-            let answer = ''
+                let answer = ''
 
-            await Promise.all(paragraphs.map(async paragraph => {
-                await paragraph.getText()
-                    .then(value => {
-                        answer += `${value} `
-                    })
+                await Promise.all(paragraphs.map(async paragraph => {
+                    await paragraph.getText()
+                        .then(value => {
+                            answer += `${value} `
+                        })
+                }))
+                answers.push(`${i + 1}) ${answer}`)
             }))
-            answers.push(`${i + 1}) ${answer}`)
-        }))
 
-        // CHECK IF QUESTION HAS MORE THAN ONE ANSWER TO CHOOSE
-        let isMultiQuiz
+            // CHECK IF QUESTION HAS MORE THAN ONE ANSWER TO CHOOSE
+            let isMultiQuiz
 
-        try {
-            isMultiQuiz = await driver.findElement(By.css('.test-multiquiz-save-line span')).isDisplayed()
-        } catch (err) {
-            isMultiQuiz = false
+            try {
+                isMultiQuiz = await driver.findElement(By.css('.test-multiquiz-save-line span')).isDisplayed()
+            } catch (err) {
+                isMultiQuiz = false
+            }
+
+            // TEMPLATES FOR ChatGPT
+            const templateOne = `Напиши правильний варіант цифрою. Запитання: "${question}". Відповіді на запитання: ${answers}`
+            const templateMany = `Напиши усі правильні варіанти цифрами. Запитання: "${question}". Відповіді на запитання: ${answers}`
+
+            // SEND REQUEST TO ChatGPT AND GET RESPONSE
+            const data = await runPrompt(isMultiQuiz ? templateMany : templateOne)
+            const rightAnswers = data.replace(/\D/g, '').split('')
+
+            // SOME ACTIONS ON WEBPAGE
+            try {
+                await Promise.all(rightAnswers.map(async rightAnswer => {
+                    await elements[Number(rightAnswer) - 1].click()
+                }))
+                    .then(async () => {
+                        if (isMultiQuiz) {
+                            await driver.findElement(By.className('test-multiquiz-save-button')).click()
+                        }
+                    })
+            } catch (err) {
+                console.log(err)
+            }
+
+            console.log(`
+                CURRENT QUESTION IS ${currentQuestion}. ${question} \n
+                RIGHT ANSWERS: ${rightAnswers} \n
+                [END OF ${currentQuestion} QUESTION]
+            `)
         }
 
-        // TEMPLATES FOR ChatGPT
-        const templateOne = `Обери одну правильну відповідь, напиши тільки цифру. Питання:"${question}". Варіанти відповідей: ${answers}`
-        const templateMany = `Обери декілька правильних відповідей, напиши тільки цифрами правильні відповіді через кому. Питання:"${question}". Варіанти відповідей: ${answers}`
+        // CHECK CURRENT QUESTION AND COMPARE
+        // IF TRUE CALL FUNCTION AGAIN
+        await setInterval(async () => {
+            await driver.findElement(By.className('currentActiveQuestion')).getText()
+                .then(async data => {
+                    const number = Number(data)
+                    if (number === currentQuestion) {
+                        return
+                    }
 
-        // SEND REQUEST TO ChatGPT AND GET RESPONSE
-        const data = await runPrompt(isMultiQuiz ? templateMany : templateOne)
-        const rightAnswers = data.replace(/\D/g, '').split('')
-
-        // SOME ACTIONS ON WEBPAGE
-        await Promise.all(rightAnswers.map(async (rightAnswer) => {
-            await elements[Number(rightAnswer) - 1].click()
-        }))
-            .then(async () => {
-                if (isMultiQuiz) {
-                    await driver.findElement(By.className('test-multiquiz-save-button')).click()
-                }
-            })
-
-        console.log(`Multiquiz is ${isMultiQuiz}`)
-        console.log(question)
-        console.log(answers)
-        console.log(`Right answers: ${rightAnswers}`)
+                    currentQuestion = number
+                    await fn()
+                })
+        }, 1500)
+    } catch (err) {
+        console.log(err)
     } finally {
-        // await driver.quit()
+        setTimeout(async () => {
+            await driver.quit()
+        }, 1000000)
     }
 }
 
