213e40a8a57552c55d7adaf984272692a2e7a8cf
yasharusakov <rusakov.yasha@gmail.com>
2023-03-11 16:02:48 +0200

    Учучшил код

--- test/lookingForTest.js
+++ test/lookingForTest.js
@@ -10,16 +10,16 @@ import {Builder, By, Key} from 'selenium-webdriver'
 import {Options} from 'selenium-webdriver/firefox.js'
 import {getRandom as createRandomUserAgent} from 'random-useragent'
 
-let options = new Options();
-let driver = await new Builder().forBrowser('firefox').build()
+let options = new Options()
 
 // Нужно что бы он создавал рандомные имена
 const names = ["abama", "jfkas;ld", "sjdhjklhskjd", "dhaj", "dfjalsjk", "jahkjk"]
 
 const sokets = [9050, 9052, 9053, 9054]
-const randomSock = sokets[Math.floor(Math.random() * sokets.length)]
+const randomValue = (arr, max) => Math.floor(Math.random() * (max ? max : arr.length))
+const randomSock = sokets[randomValue(sokets)]
 
-options.setPreference('network.proxy.type', 1) 
+options.setPreference('network.proxy.type', 1)
 options.setPreference('network.proxy.socks', '127.0.0.1')
 options.setPreference('network.proxy.socks_port', randomSock)
 options.setPreference('network.proxy.socks_remote_dns', true)
@@ -27,24 +27,38 @@ options.setPreference('network.proxy.socks_version', 5)
 
 options.setPreference('general.useragent.override', createRandomUserAgent())
 
-let code
-
-await driver.get("https://naurok.com.ua/test/join")
+let code = ''
 
 async function main() {
-    try {
-        // Оно не всегда генерирует 7-ми значное число. Нужно исправить.
-        code = (Math.random() * 10**7).toFixed(0)
-        await driver.findElement(By.id('joinform-gamecode')).sendKeys(code, Key.ENTER)
-        await driver.findElement(By.id('joinform-name')).sendKeys(names[Math.floor(Math.random() * names.length)], Key.ENTER)
-        await driver.findElement(By.css('btn btn-orange btn-lg btn-block join-button-test')).click()
-        console.log(code)
-    } catch {
-    } finally {
-        await driver.findElement(By.id('joinform-gamecode')).clear()
-        await driver.findElement(By.id('joinform-name')).clear()
+    const driver = await new Builder()
+        .forBrowser('firefox')
+        .build()
+
+    await driver.get("https://naurok.com.ua/test/join")
+
+    const test = async () => {
+        try {
+            // Генерирует 7 значное число
+            while (code.length < 7) {
+                code += randomValue(null, 9)
+            }
+
+            const randomName = names[randomValue(names)]
+
+            await driver.findElement(By.id('joinform-gamecode')).sendKeys(code, Key.ENTER)
+            await driver.findElement(By.id('joinform-name')).sendKeys(randomName, Key.ENTER)
+            // Когда используешь css, обезательно перед названием класса или айди пишишь . и #
+            await driver.findElement(By.css('.btn .btn-orange .btn-lg .btn-block .join-button-test')).click()
+        } catch (err) {
+        } finally {
+            await driver.findElement(By.id('joinform-gamecode')).clear()
+            await driver.findElement(By.id('joinform-name')).clear()
+            console.log(code)
+            code = ''
+        }
     }
+
+    setInterval(test, 2000)
 }
 
-// Нужно ставить минимум 2 скунду, а то оно не работает.
-setInterval(main, 2000)
+main()
